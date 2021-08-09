a58d79dd38f1543f0a9e7a7840504be488f735f6
Jonathan Zhou <jonathan.zhou@jzhou-ltm1.internal.salesforce.com>
2021-08-09 11:33:25 -0700

    added privacy center

--- orgInit.sh
+++ orgInit.sh
@@ -11,6 +11,8 @@ sfdx force:package:install -p 04t1t000003DLALAA4 -w 10
 # Install data mask
 sfdx force:package:install -w 20 -r -p 04t3k000001yuer
 
+# Install Privacy Center
+sfdx force:package:install --package=04t3t000002zQ6IAAU --wait 15 
 
 # Updates the Transaction Security Policies to contain this scratch orgs username
 sfdx shane:tsp:username:update
@@ -24,6 +26,8 @@ sfdx force:user:permset:assign -n MobileSecurity
 sfdx force:user:permset:assign -n Event_Monitoring_Access
 sfdx shane:user:psl -l User -g User -n datamask_DataMaskUserPsl
 sfdx shane:user:permset:assign -l User -g User -n datamask
+sfdx shane:user:psl -n 'Privacy Center User' -g User -l User
+sfdx force:user:permset:assign -n PrivacyCenter
 
 # Set the default password.
 sfdx shane:user:password:set -g User -l User -p salesforce1
